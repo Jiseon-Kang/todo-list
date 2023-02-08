1569f2cd40add2a5d408747c829158ecd7b4e7cd
Jiseon-Kang <sunzz247@gmail.com>
2023-02-08 13:38:10 +0900

    Add TODO 항목 추가 기능

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -19,6 +19,24 @@ describe('App Tests', () => {
 
         expect(screen.getByText('Hello World')).toBeInTheDocument()
     })
+
+    it('입력창에 문자를 입력 후 버튼을 누르면 입력했던 값은 창에서 지워진다.', async () => {
+        render(<App/>)
+        await userEvent.type(screen.getByRole("textbox"), 'Hello World')
+
+        await userEvent.click(screen.getByRole('button'))
+
+        expect(screen.getByRole('textbox')).toHaveValue('')
+    })
+
+    it('입력창에 문자를 입력 후 엔터 키를 누르면 입력한 내용을 화면에서 볼 수 있다.', async () => {
+        render(<App/>)
+        await userEvent.type(screen.getByRole('textbox'), 'Hello World')
+
+        await userEvent.type(screen.getByRole('textbox'), '{enter}')
+
+        expect(screen.getByText('Hello World')).toBeInTheDocument()
+    })
 })
 
 
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {ChangeEvent, useState} from 'react';
 import './App.css';
+import axios from "axios";
 
 function App() {
+
+    const [todoList, setTodoList] = useState<string[]>(['Hello', "World"])
+    const [todo, setTodo] = useState<string>('')
+
+    const changeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        setTodo(event.target.value)
+    }
+
+    const addTodo = () => {
+        // const result = []
+        // for(let index = 0 ; index < todoList.length; index++) {
+        //     result.push(todoList[index])
+        // }
+        // result.push(todo)
+        // setTodoList(result)
+        setTodoList([...todoList, todo])
+        setTodo('')
+    }
+
+    const keyDownHandler = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === "Enter") {
+            addTodo()
+        }
+    }
+
     return (
         <div className="App">
-            <input type="text" value={'Hello World'}/>
-            <button/>
+            <input type="text"
+                   onKeyDown={keyDownHandler}
+                   onChange={changeHandler} value={todo}/>
+            <button onClick={addTodo}>
+                ADD
+            </button>
+            {todoList.map((todo, index) => <div key={index}>{todo}</div>)}
         </div>
     );
 }
